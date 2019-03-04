61159bd14bd1626004f92905df5e841108738385
Nicola De Cao <nicola.decao@uva.nl>
2019-03-04 11:36:34 +0100

    new blocks

--- models/blocks.py
+++ models/blocks.py
@@ -239,8 +239,8 @@ class HouseholderLinear(torch.nn.Module):
         try:
             return torch.chain_matmul(*torch.eye(self.in_features).to(t.device) - t)
         except:
-            out = torch.eye(self.in_features).to(t.device)
-            for m in [torch.eye(self.in_features).to(t.device) - t]:
+            out = torch.eye(self.in_features).cuda()
+            for m in [torch.eye(self.in_features).cuda() - t]:
                 out = torch.matmul(out, m)
 
             return out
